13d5a3d51ee692b8a7135d85b0f7d200daefd523
Raken <rakenmaharjan1@gmail.com>
2026-09-13 15:20:13 -0500

    Update: Incorporate car traffic in the sim

--- controller.py
+++ controller.py
@@ -3,10 +3,14 @@ import time
 
 def run_simulation():
     # Start the SUMO GUI and load the 4-way intersection network
-    traci.start(["sumo-gui", "-n", "baseline.net.xml"])
+    traci.start([
+        "sumo-gui", 
+        "-n", "baseline.net.xml", 
+        "-r", "traffic.rou.xml"
+    ])
     
     step = 0
-    # Run a blank simulation for 500 steps to verify the fixed-time phases
+    # Run simulation for 500 steps
     while step < 500:
         traci.simulationStep()
         time.sleep(0.05) # Slows down the GUI to watch the lights change
