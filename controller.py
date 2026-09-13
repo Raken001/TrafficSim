1c44e6a125f3a551ba0cf9df44ff74254f21d38d
Raken <rakenmaharjan1@gmail.com>
2026-09-13 14:35:42 -0500

    Fix: Comments

--- controller.py
+++ controller.py
@@ -2,14 +2,14 @@ import traci
 import time
 
 def run_simulation():
-    # Start the SUMO GUI and load your 4-way intersection network
+    # Start the SUMO GUI and load the 4-way intersection network
     traci.start(["sumo-gui", "-n", "baseline.net.xml"])
     
     step = 0
     # Run a blank simulation for 500 steps to verify the fixed-time phases
     while step < 500:
         traci.simulationStep()
-        time.sleep(0.05) # Slows down the GUI so you can watch the lights change
+        time.sleep(0.05) # Slows down the GUI to watch the lights change
         step += 1
         
     traci.close()
